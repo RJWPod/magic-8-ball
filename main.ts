e8c78bfaecf2758a21312540cea9322b6d21d201
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-31 08:26:43 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,18 @@ input.onGesture(Gesture.Shake, function () {
     Random_Variable = randint(0, 2)
     if (Random_Variable == 0) {
         basic.showString("Maybe")
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(311, music.beat(BeatFraction.Whole))
     }
     if (Random_Variable == 1) {
         basic.showString("No")
+        music.playTone(165, music.beat(BeatFraction.Whole))
+        music.playTone(131, music.beat(BeatFraction.Whole))
     }
     if (Random_Variable == 2) {
         basic.showString("Yes")
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.playTone(698, music.beat(BeatFraction.Whole))
     }
     Random_Variable = 0
 })
@@ -21,6 +27,6 @@ basic.showLeds(`
     . # # # .
     `)
 basic.pause(100)
-basic.showString("Ask a question")
+basic.showString("Ask")
 basic.pause(100)
-basic.showString("Shake to get response ")
+basic.showString("then shake")
